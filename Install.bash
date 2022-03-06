7749d2c831440816d3b77c5556f07d06e76c25d9
NateYeet <95325855+NateYeet@users.noreply.github.com>
2022-03-05 21:32:08 -0500

    Create Install.bash

---
+++ Install.bash
@@ -0,0 +1,9 @@
+clear
+echo Installing Benvabuntu...
+mkdir ~/Benvabuntu
+touch ~/Benvabuntu/.Benva
+sudo apt isntall code
+code ~/Benvabuntu/.Benva
+alias cls='clear'
+alias welcome='echo "Welcome $USER."'
+alias Benva='echo "Help is avalable at "'
