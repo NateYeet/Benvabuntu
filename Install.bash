43700f2270e7c470445cf0fe534422c5e9ed13b1
NateYeet <95325855+NateYeet@users.noreply.github.com>
2022-03-05 21:40:35 -0500

    Delete Install.bash

--- Install.bash
+++
@@ -1,9 +0,0 @@
-clear
-echo Installing Benvabuntu...
-mkdir ~/Benvabuntu
-touch ~/Benvabuntu/.Benva
-sudo apt isntall code
-code ~/Benvabuntu/.Benva
-alias cls='clear'
-alias welcome='echo "Welcome $USER."'
-alias Benva='echo "Help is avalable at "'
