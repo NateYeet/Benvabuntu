e7b480325383c7927fcd464f8a23fcaddbc3c4eb
NateYeet <95325855+NateYeet@users.noreply.github.com>
2022-03-05 21:40:49 -0500

    Add files via upload

---
+++ Benvabuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+echo Installing Benvabuntu...
+mkdir ~/Benvabuntu
+touch ~/Benvabuntu/.Benva
+sudo apt isntall code
+code ~/Benvabuntu/.Benva
+alias cls='clear'
+alias welcome='echo "Welcome $USER."'
+alias benva-help='echo "Help is avalable at https://github.com/NateYeet/Benvabuntu"'
+alias benva-settings='cls'
+source ~/Benvabuntu/.Benva
