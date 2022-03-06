d18e6734ab3221953fc91cd57a6c6a0ee17e547e
NateYeet <95325855+NateYeet@users.noreply.github.com>
2022-03-06 08:41:57 -0500

    Add files via upload

---
+++ .BenvaCommands.sh
@@ -0,0 +1,6 @@
+alias cls='clear'
+alias welcome='echo "Welcome $USER."'
+alias benva-help='echo "Help is avalable at https://github.com/NateYeet/Benvabuntu"'
+alias benva-settings='cls'
+source ~/Benvabuntu/.BenvaCommands.sh
+cls
